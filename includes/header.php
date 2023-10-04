0691f491ab931d9445b9097b773e9d89035ee79f
Ravindu Amarasekara <rav.business.lak@gmail.com>
2023-10-04 19:56:55 +0530

    1st

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+<header>
+
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand Anton fs-4 text-info border border-4 border-info rounded p-1" href="#">
+                <span class="mx-1"><img src="img/icons8-engineering-50.png" alt="" class="img-fluid rotating-element" style="height: 30px;"> Remote Electric Bill</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse ms-2 fs-5 tillium" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-3 y">
+                        <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-window-fullscreen"></i> Dashborad</a>
+                    </li>
+                    <li class="nav-item mx-3 y">
+                        <a class="nav-link" href="admin_panel_P.html"><i class="bi bi-diagram-2-fill"></i> Utilities</a>
+                    </li>
+                    <li class="nav-item mx-3 y">
+                        <a class="nav-link" href="admin_panel_U.html"><i class="bi bi-people"></i> Payments</a>
+                    </li>
+                    <li class="nav-item dropdown mx-3 y">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="letter-spacing: 1px;">
+                            <i class="bi bi-person-badge"></i> Ravindu Amarasekara
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" style="background-color: #585858;">
+                            <li>
+                                <a class="dropdown-item" href="admin_panel_SignIn.html"><i class="bi bi-door-open-fill me-1"></i>Sign In</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="admin_panel_Register.html"><i class="bi bi-person-plus-fill me-1"></i>Register</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"><i class="bi bi-person-fill-gear me-1"></i>
+                                    Account Settings</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
